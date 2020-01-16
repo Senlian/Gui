21ff738b1a355ac5f1575d9317291fc8f4a5a93c
SenLian <719989919@qq.com>
2020-01-16 15:19:56 +0800

    sort

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import gc
 import multiprocessing
 from gui.root import NewApp
 
-# venv\Scripts\pyinstaller.exe -F -i E:\GitHub\py3\GameProcessManager\media\icons\poker.ico -n ProcessManager main.py
+# venv\Scripts\pyinstaller.exe --clean -w -F -i E:\GitHub\py3\GameProcessManager\media\icons\poker.ico -n ProcessManager main.py
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     app = NewApp()
